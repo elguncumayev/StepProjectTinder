c9747e5789e523ee84808b21b822f334b0b55a14
RealSerkhan <60697846+RealSerkhan@users.noreply.github.com>
2020-05-24 23:07:58 +0400

    #1

--- src/main/java/dao/DAOUsersSql.java
+++ src/main/java/dao/DAOUsersSql.java
@@ -27,29 +27,98 @@ public class DAOUsersSql implements DAO<User> {
 
   @Override
   public boolean check(String query) {
-    throw new RuntimeException("Not implemented");
+    try {
+      Connection conn = DriverManager.getConnection(URL, UNAME, PWD);
+      PreparedStatement stmt = conn.prepareStatement(query);
+      ResultSet rSet = stmt.executeQuery();
+      return rSet.next();
+    } catch (SQLException e) {
+      return false;
+    }
   }
 
   @Override
   public Collection<User> getBySQLQuery(String query) {
-    throw new RuntimeException("Not implemented");
+    try {
+      Connection conn = DriverManager.getConnection(URL, UNAME, PWD);
+      return getUsers(conn, query);
+    } catch (SQLException e) {
+      return new ArrayList<>();
+    }
   }
 
   @Override
   public int getID(String query) {
-    throw new RuntimeException("Not implemented");
+    try {
+      Connection conn = DriverManager.getConnection(URL, UNAME, PWD);
+      PreparedStatement stmt = conn.prepareStatement(query);
+      ResultSet rSet = stmt.executeQuery();
+      rSet.next();
+      return rSet.getInt("id");
+    } catch (SQLException e) {
+      return -1;
+    }
   }
 
   @Override
   public Optional<User> get(int id) {
-    throw new RuntimeException("Not implemented");
+    try {
+      Connection conn = DriverManager.getConnection(URL, UNAME, PWD);
+      String SQL = String.format("select * from users u where u.id = %s;", id);
+      PreparedStatement stmt = conn.prepareStatement(SQL);
+      ResultSet rSet = stmt.executeQuery();
+      rSet.next();
+      int ID = rSet.getInt("id");
+      String username = rSet.getString("e-mail");
+      String fullName = rSet.getString("fullName");
+      String lastLogin = rSet.getString("lastLogin");
+      Optional<String> workInfo = Optional.ofNullable(rSet.getString("workInfo"));
+      Optional<String> prof_photo = Optional.ofNullable(rSet.getString("prof_photo"));
+      return Optional.of(new User(
+              ID,
+              username,
+              fullName,
+              workInfo.orElse(""),
+              LocalDateTime.parse(lastLogin),
+              prof_photo.orElse(defaultPhoto)
+      ));
+    } catch (SQLException e) {
+      return Optional.empty();
+    }
   }
 
   @Override
   public boolean executeSQL(String query) {
-    throw new RuntimeException("Not implemented");
+    try {
+      Connection conn = DriverManager.getConnection(URL, UNAME, PWD);
+      PreparedStatement stmt = conn.prepareStatement(query);
+      stmt.execute();
+      return true;
+    } catch (SQLException e) {
+      return false;
+    }
   }
 
   private Collection<User> getUsers(Connection conn, String SQL) throws SQLException {
-    throw new RuntimeException("Not implemented");
+    PreparedStatement stmt = conn.prepareStatement(SQL);
+    ResultSet rSet = stmt.executeQuery();
+    ArrayList<User> users = new ArrayList<>();
+    while (rSet.next()) {
+      int ID = rSet.getInt("id");
+      String username = rSet.getString("e-mail");
+      String fullName = rSet.getString("fullName");
+      String lastLogin = rSet.getString("lastLogin");
+      Optional<String> workInfo = Optional.ofNullable(rSet.getString("workInfo"));
+      Optional<String> prof_photo = Optional.ofNullable(rSet.getString("prof_photo"));
+      users.add(new User(
+              ID,
+              username,
+              fullName,
+              workInfo.orElse(" "),
+              LocalDateTime.parse(lastLogin),
+              prof_photo.orElse(defaultPhoto)
+      ));
+    }
+    return users;
+  }
 }
